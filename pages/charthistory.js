39deeca59d5c352fac806f38f96c8872db3926cc
J8cob1 <jacob.crisan@gmail.com>
2020-05-31 14:28:07 -0700

    Continued working on chart history page. Got some charts going, but still need to fill with data, and fix page to look good and work

--- pages/charthistory.js
+++ pages/charthistory.js
@@ -1,10 +1,72 @@
 import Head from 'next/head'
 import Base from '../components/base'
+import { Bar } from 'react-chartjs-2';
 
 // Database Query Functions
 
 // A function component, since I don't know if I need anything else
 export default function ChartHistory() {
+    // https://github.com/jerairrest/react-chartjs-2/issues/388 told me how to get dataset colors
+    // https://www.chartjs.org/docs/latest/charts/bar.html#data-structure
+    // https://github.com/jerairrest/react-chartjs-2
+    // https://pusher.com/tutorials/realtime-data-visualization-nextjs
+    
+    // Might consider adding a table with data below the charts
+
+    // Dataset Variables
+    let properties = {}
+    let temperature_data = {
+        labels: ["temp 1", "temp 2", "temp 3", "temp 4", "temp 5"],
+        datasets: [{
+            label: "Temperature (degrees fahrenheit)",
+            backgroundColor: "red",
+            barThickness: 50,
+            minBarLength: 100,
+            data: [10, 20, 30, 40, 50]
+        }]
+    }
+    let wind_data = {
+        labels: ["temp 1", "temp 2", "temp 3", "temp 4", "temp 5"],
+        datasets: [{
+            label: "Avg. Wind Speed (m/s)",
+            backgroundColor: "orange",
+            barThickness: 50,
+            minBarLength: 100,
+            data: [10, 20, 30, 40, 50]
+        }]
+    }
+    let pressure_data = {
+        labels: ["temp 1", "temp 2", "temp 3", "temp 4", "temp 5"],
+        datasets: [{
+            label: "Pressure (pascals)",
+            backgroundColor: "gold",
+            barThickness: 50,
+            minBarLength: 100,
+            data: [10, 20, 30, 40, 50]
+        }]
+    }
+
+    // Fill datasets
+    /*function getData(start_date, end_date) {*/
+        // date
+        // temp
+        // avg. wind speed
+        // pressure
+
+    // Charts.js variables
+
+    // Get data for start and end date, and parse it
+    /*let results = {start_date, end_date} // Make SQL Call
+    for (day in results) {
+        // Append date for bar graph labels
+        labels.push(day.date);
+        
+        // Append dataset information
+        temp_dataset.data.push();
+        wind_dataset.data.push();
+        pressure_dataset.push();
+    }*/
+
     return (
         <>
         <Head>
@@ -18,7 +80,7 @@ export default function ChartHistory() {
               <label>Start Date</label>
               <input type="date"/>
               <label>End Date</label>
-              <input type="date"/>
+              <input type="dsate"/>
               <input type="submit" value="Draw Charts"/>
             </form>
             <hr/>
@@ -30,14 +92,18 @@ export default function ChartHistory() {
              </p>
           </div>
           <div className="charts">
-    
+            <div>
+                <Bar options={{ maintainAspectRatio: false }} data={temperature_data} height={200} width={300}/>
+            </div>
+            <div>
+                <Bar options={{ maintainAspectRatio: false }} data={wind_data} height={200} width={300}/>
+            </div>
+            <div>
+                <Bar options={{ maintainAspectRatio: false }} data={pressure_data} height={200} width={300}/>
+            </div>
           </div>
           <style jsx>{`
             /*
-            <Chart type="temp"></Chart>
-            <Chart type="wind-speed"></Chart>
-            <Chart type="wind-direction"></Chart>
-            <Chart type="pressure"></Chart>
             */
             h1 {
               border-bottom: 1px solid black; 
@@ -50,13 +116,18 @@ export default function ChartHistory() {
               flex-wrap: wrap;
               justify-content: space-around;
             }
+            .charts {
+                margin: 10px;
+            }
+            form {
+                width: 45%;
+            }
             form input[type="submit"] {
-              width: 50%;
+              width: 100%;
             }
           `}
           </style>
         </Base>
-
         </>
     )
 }
